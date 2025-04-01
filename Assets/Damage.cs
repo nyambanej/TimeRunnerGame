bbe590bdfafc801d975d3a78dc128e5bdda97482
EdrianVillegas <janedrianvillegas@yahoo.com>
2025-04-01 03:10:54 -0600

    Added player health and health bar ; updated  spiked mines damge

---
+++ Assets/Damage.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Damage : MonoBehaviour
+{
+    
+    public float damage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // (Optional) Initialize any variables here
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // (Optional) Logic every frame
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        // If the colliding object is tagged "Player", deal damage
+        if (other.gameObject.CompareTag("Player"))
+        {
+            other.gameObject.GetComponent<PlayerHealth>().health -= damage;
+
+        }
+    }
+}
