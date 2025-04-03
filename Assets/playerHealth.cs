2580b7c737a9c7621ea88e19eec1b942d8c3e12a
EdrianVillegas <janedrianvillegas@yahoo.com>
2025-04-02 22:28:22 -0600

    Added Gameover UI and UI button function

--- Assets/playerHealth.cs
+++ Assets/playerHealth.cs
@@ -9,6 +9,9 @@ public class PlayerHealth : MonoBehaviour
     public float maxHealth;
     public Image healthBar;
 
+    private bool isDead;
+
+    public GameManagerScript gameManager;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,8 +23,10 @@ public class PlayerHealth : MonoBehaviour
     {
         healthBar.fillAmount = Mathf.Clamp(health / maxHealth, 0, 1);
 
-        if (health <= 0)
+        if (health <= 0 && !isDead)
         {
+            isDead = true;
+            gameManager.gameOver();
             Destroy(gameObject);
         }
 
