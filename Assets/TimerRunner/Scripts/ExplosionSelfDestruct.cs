283400e7577b0aaf200e2568a33c1c253a15b623
Tyler Lang <tyleer.lang@gmail.com>
2025-04-04 21:29:30 -0600

    Added sound effect for spiked mines

--- Assets/TimerRunner/Scripts/ExplosionSelfDestruct.cs
+++ Assets/TimerRunner/Scripts/ExplosionSelfDestruct.cs
@@ -3,9 +3,16 @@ using UnityEngine;
 public class ExplosionSelfDestruct : MonoBehaviour
 {
     public float destroyAfter = 1f; // Match your explosion animation duration
+    public AudioSource explosionSound; // Drag this in from the prefab
 
     void Start()
     {
+
+        if (explosionSound != null)
+        {
+            explosionSound.Play();
+        }
+
         Destroy(gameObject, destroyAfter);
     }
 }
